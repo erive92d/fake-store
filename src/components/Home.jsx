904632aba6c5249f3cd8e4a97487dfa89dca1db9
Deorren Erive <mac@Deorrens-Air.attlocal.net>
2023-04-30 18:45:14 -0700

    added category

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,25 +1,37 @@
 import { Navtab } from "./Navtab";
-import { allProducts } from "../utils/API"
+import { allProducts, getCategory } from "../utils/API"
 import { useEffect, useState } from "react";
 import { DisplayItems } from "../pages/DisplayItems";
 
 export default function Home() {
 
     const [items, setItems] = useState([])
-
-
+    const [currentAPI, setCurrentAPI] = useState(allProducts())
 
     useEffect(() => {
-        allProducts()
+        currentAPI
             .then((res) => res.json())
             .then((data) => setItems(data))
-    }, [])
-    console.log(items)
+    }, [currentAPI])
 
+    const handleClick = (e) => {
+
+        e.preventDefault()
+        const catName = e.target.value
+
+        setCurrentAPI(getCategory(catName))
+    }
+    console.log(currentAPI)
     return (
-        <>
+        <div>
+            <div className="flex justify-around border">
+                <button onClick={handleClick} value="jewelery">Jewelry</button>
+                <button onClick={handleClick} value="men's clothing">Mens</button>
+                <button onClick={handleClick} value="electronics">Electronics</button>
+            </div>
+
             <DisplayItems items={items} />
 
-        </>
+        </div>
     )
 }
