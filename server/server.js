b97f7eaf85fc3c64c328eca10709c9380d9419dc
Deorren Erive <erive92d@gmail.com>
2023-05-02 13:04:33 -0700

    s

--- server/server.js
+++ server/server.js
@@ -11,13 +11,17 @@ app.use(express.json());
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../dist/index.html")));
+  app.use(express.static(path.join(__dirname, "../dist")));
 }
 
 // app.get("*", (req, res) => {
 //   res.sendFile(path.join(__dirname, "../dist/index.html"));
 // });
 
+// app.get("*", function (req, res) {
+//   // res.sendFile(path.resolve('client', 'build' , 'index.html'));
+//   res.sendFile(path.resolve(__dirname, "../dist/index.html"));
+// });
 app.use(routes);
 
 db.once("open", () => {
