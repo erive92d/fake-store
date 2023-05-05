45081748f604bb997188bcf727e295af31d19e80
Deorren Erive <erive92d@gmail.com>
2023-05-05 06:13:22 -0700

    s

--- server/server.js
+++ server/server.js
@@ -10,14 +10,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === "production") {
-  res.sendFile(path.join(__dirname, "../dist", "../index.html"));
-}
-
-// app.get("/", (req, res) => {
+// if (process.env.NODE_ENV === "production") {
 //   res.sendFile(path.join(__dirname, "../dist", "../index.html"));
-// });
-app.use(express.static(path.join(__dirname, "dist", "index.html")));
+// }
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../dist", "../index.html"));
+});
+// app.use(express.static(path.join(__dirname, "dist", "index.html")));
 
 app.use(routes);
 const host = "0.0.0.0";
