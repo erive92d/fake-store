b5fbb48d9c58441e94e94cf65facf6253c11983e
Deorren Erive <erive92d@gmail.com>
2023-05-05 06:08:21 -0700

    s

--- server/server.js
+++ server/server.js
@@ -14,10 +14,10 @@ app.use(express.json());
 //   app.use(express.static(path.join(__dirname, "dist")));
 // }
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../dist", "../index.html"));
-});
-// app.use(express.static(path.join(__dirname, "dist", "index.html")));
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../dist", "../index.html"));
+// });
+app.use(express.static(path.join(__dirname, "dist", "index.html")));
 
 app.use(routes);
 const host = "0.0.0.0";
