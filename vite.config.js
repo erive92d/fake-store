7047537c7fe2062f70f46b0e33835ccd1b481b12
Deorren Erive <erive92d@gmail.com>
2023-05-06 12:20:12 -0700

    s

--- vite.config.js
+++ vite.config.js
@@ -5,19 +5,19 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   // base: "./",
-  // server: {
-  //   // host: true,
-  //   // port: "8080",
-  //   // hot: true,
-  //   proxy: {
-  //     "/api": {
-  //       target: "http://0.0.0.0:5000/",
-  //       changeOrigin: true,
-  //       secure: false,
-  //       ws: true,
-  //     },
-  //   },
-  // },
+  server: {
+    // host: true,
+    // port: "8080",
+    // hot: true,
+    proxy: {
+      "/api": {
+        target: "http://0.0.0.0:5000/",
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+      },
+    },
+  },
   build: {
     outDir: "dist",
     assetsDir: "static",
