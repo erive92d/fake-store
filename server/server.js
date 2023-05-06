7ceb5f908a5fdda3450217b177db59c513093702
Deorren Erive <erive92d@gmail.com>
2023-05-06 12:00:42 -0700

    s

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("dist", "index.html"));
+  app.use(express.static("dist"));
   // app.get("*", (req, res) => {
   //   // res.sendFile(path.join(__dirname, "../index.html"));
   //   // res.sendFile(path.join(__dirname, "/dist/index.html"));
