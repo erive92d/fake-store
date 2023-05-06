2cbb3d0e40686d8568ed073ba5f50c5a00a8a858
Deorren Erive <erive92d@gmail.com>
2023-05-06 06:45:07 -0700

    s

--- server/server.js
+++ server/server.js
@@ -15,7 +15,8 @@ if (process.env.NODE_ENV === "production") {
   app.get("*", (req, res) => {
     // res.sendFile(path.join(__dirname, "../index.html"));
     // res.sendFile(path.join(__dirname, "/dist/index.html"));
-    res.sendFile(path.join(__dirname, "../dist/index.html"));
+    // res.sendFile(path.join(__dirname, "../dist/index.html"));
+    res.sendFile(path.join(__dirname, "../dist"));
   });
 }
 
