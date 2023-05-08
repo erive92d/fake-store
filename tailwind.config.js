ff5bc824728e43034da9427a7af10d0275852144
Deorren Erive <erive92d@gmail.com>
2023-05-08 08:35:50 -0700

    s

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ export default {
   content: [
     // "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/flowbite/**/*.js",
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
