f3279b195fe01b575da16c4be0758048d3b6acae
erive92d <erive92d@gmail.com>
2024-02-28 18:30:58 -0800

    fixed fetching

--- src/components/cartcomps/CartItems.jsx
+++ src/components/cartcomps/CartItems.jsx
@@ -4,7 +4,7 @@ export default function CartItems({ ...items }, key) {
     return (
         <li className="border-b" key={key}>
             <span className='font-bold'>{items.item.title}</span>
-            <span className=''>{items.size.toUpperCase()}</span>
+            <span className=''>{items.size?.toUpperCase()}</span>
             <img className='w-3/4 h-60' src={items.item.image} />
         </li>
     )
